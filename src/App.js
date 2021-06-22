4a8b1c5bd77d687147105517264518db2bf0a895
nazik310104 <ms.naziku04@gmail.com>
2021-06-22 17:03:38 +0600

    do adaptive

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Button from "./components/DecodeToken.js";
+import HeroCarousel from "./components/HeroCarousel/HeroCarousel.js";
 function App() {
   return <div className="App"></div>;
 }
