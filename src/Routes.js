e36e01ec83d2ce04f1c74fa84a926590ed17ccaf
Aziz K <cosmonaut.aziz@gmail.com>
2021-06-24 17:37:40 +0600

    chnaged old signup routes

--- src/Routes.js
+++ src/Routes.js
@@ -7,36 +7,36 @@ import Home from "./Views/Home/Home";
 import Promotions from "./Views/Promotions/Promotions";
 import Sessions from "./Views/Sessions/Sessions";
 import Showing from "./Views/Showing/Showing";
-import Signup from "./components/authentication/Server/SignUpWithServer/SignUpWithServer";
+import SignUpServer from "./components/authentication/Server/SignUpWithServer/SignUpWithServer";
 import Login from "./components/authentication/Firebase/Login/Login";
 import ForgotPassword from "./components/authentication/Firebase/ForgotPassword/ForgotPassword";
 import { AuthProvider } from "./contexts/AuthContext";
 import CreateMovie from "./Views/CreateMovie/CreateMovie";
 import MovieDetailPage from "./Views/MovieDetailPage/MovieDetailPage";
-import SignUp from "./components/authentication/Server/SignUpWithServer/SignUpWithServer";
+import SignupFirebase from "./components/authentication/Firebase/SignupFirebase/SignupFirebase";
 import SignIn from "./components/authentication/Server/SignInWithServer/SignInWithServer";
 
 export default function Routes() {
-  return (
-    <Router>
-      <AuthProvider>
-        <Switch>
-          <Route path="/favorite" component={Favorite} />
-          <Route path="/signup" component={Signup} exact />
-          <Route path="/login" component={Login} />
-          <Route path="/signUpServer" component={SignUp} exact />
-          <Route path="/signInServer" component={SignIn} exact />
-          <Route path="/forgot-password" component={ForgotPassword} exact />
-          <Route path="/" component={Home} exact />
-          <Route path="/showing" component={Showing} exact />
-          <Route path="/about" component={About} exact />
-          <Route path="/sessions" component={Sessions} exact />
-          <Route path="/contacts" component={Contacts} exact />
-          <Route path="/promotions" component={Promotions} exact />
-          <Route path="/create-movie" component={CreateMovie} exact />
-          <Route path="/movies/:id" component={MovieDetailPage} exact />
-        </Switch>
-      </AuthProvider>
-    </Router>
-  );
+    return (
+        <Router>
+            <AuthProvider>
+                <Switch>
+                    <Route path="/favorite" component={Favorite} exact />
+                    <Route path="/signup" component={SignupFirebase} exact />
+                    <Route path="/login" component={Login} exact />
+                    <Route path="/signUpServer" component={SignUpServer} exact />
+                    <Route path="/signInServer" component={SignIn} exact />
+                    <Route path="/forgot-password" component={ForgotPassword} exact />
+                    <Route path="/" component={Home} exact />
+                    <Route path="/showing" component={Showing} exact />
+                    <Route path="/about" component={About} exact />
+                    <Route path="/sessions" component={Sessions} exact />
+                    <Route path="/contacts" component={Contacts} exact />
+                    <Route path="/promotions" component={Promotions} exact />
+                    <Route path="/createMovie" component={CreateMovie} exact />
+                    <Route path="/movies/:id" component={MovieDetailPage} exact />
+                </Switch>
+            </AuthProvider>
+        </Router>
+    );
 }
