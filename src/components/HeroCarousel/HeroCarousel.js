4677afe744604e4d2630fa7a9e75ab923c0e723e
Aziz K <cosmonaut.aziz@gmail.com>
2021-06-25 21:29:44 +0600

    added new component for SHOWING

--- src/components/HeroCarousel/HeroCarousel.js
+++ src/components/HeroCarousel/HeroCarousel.js
@@ -17,6 +17,16 @@ export default function HeroCarousel(props) {
       description: "Hello World!",
       img: "https://images.squarespace-cdn.com/content/v1/51b3dc8ee4b051b96ceb10de/1584638663178-SW29SW31WBZ7NSNB0CKW/ke17ZwdGBToddI8pDm48kNvT88LknE-K9M4pGNO0Iqd7gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z5QPOohDIaIeljMHgDF5CVlOqpeNLcJ80NK65_fV7S1USOFn4xF8vTWDNAUBm5ducQhX-V3oVjSmr829Rco4W2Uo49ZdOtO_QXox0_W7i2zEA/image-asset.jpeg?format=2500w",
     },
+    {
+      name: "Random Name #1",
+      description: "Probably the most random thing you have ever seen!",
+      img: "https://images.alphacoders.com/385/thumb-1920-3853.jpg",
+    },
+    {
+      name: "Random Name #1",
+      description: "Probably the most random thing you have ever seen!",
+      img: "https://wallpapercave.com/wp/wp3982951.jpg",
+    },
   ];
 
   return (
