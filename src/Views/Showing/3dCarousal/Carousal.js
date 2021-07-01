d9cb26971853c1d0c634be67cb1323208b525a07
Aziz K <cosmonaut.aziz@gmail.com>
2021-07-01 19:40:12 +0600

    added styles

--- src/Views/Showing/3dCarousal/Carousal.js
+++ src/Views/Showing/3dCarousal/Carousal.js
@@ -4,10 +4,9 @@ import { Carousel } from "3d-react-carousal";
 export default function Carousal() {
   let slides = [
     <img src="https://cdn.wallpapersafari.com/16/81/Z0BPWR.jpg" alt="1" />,
-    <img src="https://cdn.wallpapersafari.com/16/81/Z0BPWR.jpg" alt="2" />,
-    <img src="https://wallpapercave.com/wp/wp5435701.jpg" alt="3" />,
-    <img src="https://images3.alphacoders.com/809/809912.jpg" alt="4" />,
-    <img src="https://wallpapermemory.com/uploads/606/dwayne-johnson-wallpaper-full-hd-1920x1080-188754.jpg" alt="5" />];
+    <img src="https://wallpapercave.com/wp/wp5435701.jpg" alt="2" />,
+    <img src="https://images3.alphacoders.com/809/809912.jpg" alt="3" />,
+    <img src="https://wallpapermemory.com/uploads/606/dwayne-johnson-wallpaper-full-hd-1920x1080-188754.jpg" alt="4" />];
   return (
     <div>
       <Carousel slides={slides} autoplay={true} interval={5000} />
